9c86152749b6e9dfc18a6d9a4b554545dce1399b
Will Howes <willmhowes@gmail.com>
2021-01-18 19:28:30 -0800

    abstract logic into more functions

--- test/selenium_test.js
+++ test/selenium_test.js
@@ -16,18 +16,53 @@ var driver = new webdriver.Builder()
   .setFirefoxOptions(options)
   .build();
 
+function findPercentComplete(source) {
+  let re = /<div class="ProgressBarFill" style="width: (\d{1,3})%">/;
+  let result = re.exec(source);
+  return result[1];
+}
+
+async function takeScreenshot() {
+  const data = await driver.takeScreenshot();
+  await promisify(writeFile)('screenshot.png', data, 'base64');
+}
+
+async function nextPage() {
+  await driver.findElement(webdriver.By.name("NextButton")).click();
+}
+
 async function page1() {
-  await driver.get(`${process.env.URL}`);
+  // print page title
   let title = await driver.getTitle();
   console.log(title);
-  await driver.sleep(1000);
 
   let text = await driver.findElement(webdriver.By.className('Selection reg'));
   await text.click();
-  console.log(await text.getText());
-  const data = await driver.takeScreenshot();
-  await promisify(writeFile)('screenshot.png', data, 'base64');
+  console.log(`"${await text.getText()}"`);
+  nextPage();
+}
+
+async function main() {
+  // load page
+  await driver.get(`${process.env.URL}`);
+  await driver.sleep(1000);
+
+  // determine progress
+  let progress = 0;
+  let source = await driver.getPageSource();
+  progress = findPercentComplete(source);
+  console.log("Complete:", progress);
+
+  // activate form completion
+  if (progress == 0) {
+    page1();
+  } else if (progress == 50) {
+    // page2();
+  } else if (progress == 100) {
+    // page3();
+  }
+
   await driver.quit();
 }
 
-page1();
+main();
